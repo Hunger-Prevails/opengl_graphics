dabb20bc6009dc69eb0b3fb74ebb088af3146231
yinglun liu <yinglun.liu.it.happens@gmail.com>
2020-10-05 23:15:11 +0200

    texture display

--- main.cpp
+++ main.cpp
@@ -75,30 +75,28 @@ GLuint load_texture(string image_file)
 
 GLuint vertex_array;
 GLuint programId;
+GLuint texture;
 
-// Initialize and put everything together
 void init()
 {
-    GLuint texture = load_texture("../res/container.png");
+    texture = load_texture("../res/container.png");
 
-    // clear the framebuffer each frame with black color
     glClearColor(0.2f, 0.2f, 0.2f, 0);
 
     float vertex_position[] = {-0.5, -0.5, 0, 0.5, -0.5, 0, -0.5, 0.5, 0, 0.5, 0.5, 0};
-
     vector<float> positions;
-
     positions.assign(vertex_position, vertex_position + 12);
-
     GLuint position_buffer = loadArrayBuffer(positions);
 
     float vertex_color[] = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
-
     vector<float> colors;
-
     colors.assign(vertex_color, vertex_color + 12);
-
     GLuint color_buffer = loadArrayBuffer(colors);
+
+    float vertex_tex_coord[] = {0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0};
+    vector<float> tex_coords;
+    tex_coords.assign(vertex_tex_coord, vertex_tex_coord + 8);
+    GLuint tex_coord_buffer = loadArrayBuffer(tex_coords);
 
     GLuint element_buffer = loadElementBuffer();
 
@@ -112,27 +110,27 @@ void init()
 
     programId = linkProgram(vShaderId, fShaderId);
 
-    // Get the 'pos' variable location inside this program
     GLuint position_attrib = glGetAttribLocation(programId, "aPosition");
     GLuint color_attrib = glGetAttribLocation(programId, "aColor");
+    GLuint tex_coord_attrib = glGetAttribLocation(programId, "aTexCoord");
 
     glGenVertexArrays(1, &vertex_array); // Generate VAO  (Vertex Array Object)
 
-    // Bind it so that rest of vao operations affect this vao
     glBindVertexArray(vertex_array);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer);
 
     glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
     glVertexAttribPointer(position_attrib, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(position_attrib);
 
     glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
     glVertexAttribPointer(color_attrib, 3, GL_FLOAT, false, 0, 0);
-
-    // Enable this attribute array linked to 'pos'
-    glEnableVertexAttribArray(position_attrib);
     glEnableVertexAttribArray(color_attrib);
 
-    // unbind objects
+    glBindBuffer(GL_ARRAY_BUFFER, tex_coord_buffer);
+    glVertexAttribPointer(tex_coord_attrib, 2, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(tex_coord_attrib);
+
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -142,21 +140,20 @@ void init()
 // glut calls this function whenever it needs to redraw
 void display()
 {
-    // clear the color buffer before each drawing
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // use this program for rendering.
     glUseProgram(programId);
-    // use this vertex array object for attribute arrangement
+    
     glBindVertexArray(vertex_array);
 
     int volume_uniform = glGetUniformLocation(programId, "uVolume");
     auto time_value = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
     glUniform1f(volume_uniform, sin(time_value * 3.1416 / 2000.0f) / 2.0f + 0.5f);
 
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // swap the buffers and hence show the buffers
-    // content to the screen
+    
     glutSwapBuffers();
 }
 
--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -2,11 +2,14 @@
 
 in vec3 aPosition;
 in vec3 aColor;
+in vec2 aTexCoord;
 
 out vec3 vColor;
+out vec2 vTexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPosition, 1);
+    gl_Position = vec4(aPosition, 1.0);
     vColor = aColor;
+    vTexCoord = aTexCoord;
 }
